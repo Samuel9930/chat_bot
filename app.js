475d1df102da5c17fb8db6f11882fd34a7f76675
unknown <SenaTarde@ARMAPRRQDFSD104.FormacionSena.RED>
2024-02-27 16:39:25 -0500

    chat bot configurado

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const {API_KEY_GEMINI, START_CHAT, GENERATION_CONFIG} = require('./config')
+const { API_KEY_GEMINI, START_CHAT, GENERATION_CONFIG } = require('./config');
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(API_KEY_GEMINI);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-const app = express()
-.use(bodyParser.json())
-const port = 3000
+const app = express();
 
+// Middleware para habilitar CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir solicitudes desde cualquier origen
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos HTTP permitidos
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Cabeceras permitidas
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Responder a las solicitudes OPTIONS
+  } else {
+    next(); // Continuar con el siguiente middleware
+  }
+});
+
+app.use(bodyParser.json());
+
+const port = 3000;
 
 app.post('/chat', async (req, res) => {
   let history = req.body.history;
@@ -22,12 +35,11 @@ app.post('/chat', async (req, res) => {
   const sendQuestion = await chat.sendMessage(question);
   const response = await sendQuestion.response;
   const text = response.text();
-  history.push({role: "user", parts: question})
-  history.push({role: "model", parts: text})
-  return res.status(200).json({history: history});
-})
-
+  history.push({ role: "user", parts: question })
+  history.push({ role: "model", parts: text })
+  return res.status(200).json({ history: history });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
